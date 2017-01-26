36a8fecc85f669d512980f39b01047c77cc64b4f
Arathus <Arathus>
2017-01-26 23:36:19 +0100

    added l/r children to BTN

--- src/node.h
+++ src/node.h
@@ -5,12 +5,17 @@
 #ifndef NODE_H
 #define NODE_H
 
+#include <cstddef>
+#include <memory>
+
 
 
 template <typename T>
 class BinaryTreeNode
 {
 
+std::unique_ptr<BinaryTreeNode<T> > left_child;
+std::unique_ptr<BinaryTreeNode<T> > right_child;
 
 
 
