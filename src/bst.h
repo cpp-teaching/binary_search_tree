d20854946ecb71f706c16cfdf0df5b6c392cc5b1
Arathus <Arathus>
2017-01-26 21:03:02 +0100

    Creating bst.h class

---
+++ src/bst.h
@@ -0,0 +1,21 @@
+
+
+
+template <typename T>
+class BinarySearchTree
+{
+public:
+    BinarySearchTree(const BinarySearchTree& other) = delete;
+    BinarySearchTree(BinarySearchTree&& other) = delete;
+    BinarySearchTree operator =(const BinarySearchTree& other) = delete;
+    BinarySearchTree& operator =(BinarySearchTree&& other) = delete;
+
+
+
+
+
+
+
+};
+
+
